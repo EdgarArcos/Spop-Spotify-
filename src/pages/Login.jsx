2eb807a14caa3450cdd3373b6a4896322bd00c1f
Edgar Arcos <edgararcosmartin@gmail.com>
2023-03-16 17:13:40 +0000

    Register colocado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+      <div className='flex justify-center items-center h-screen'>
+        <div className='text-center'>
+        <h2>Create an account</h2>
+        <div>
+          <p>Name:</p>
+          <input type="text"/>
+        </div>
+        <div>
+          <p>Email:</p>
+          <input type="text"/>
+        </div>
+        <div>
+          <p>Password:</p>
+          <input type="text"/>
+        </div>
+        <div>
+          <p>Confirm password:</p>
+          <input type="text"/>
+        </div>
+        <div>
+          <button className='bg-indigo-500' >Create</button>
+        </div>
+        </div>
+        </div>
   )
 }
