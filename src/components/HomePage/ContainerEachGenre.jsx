ddc3b74fb4b71584df5244b3f68247542d0dc717
Isaura Martí <imarti.teixido@gmail.com>
2023-03-17 12:52:14 +0100

    change font family

--- src/components/HomePage/ContainerEachGenre.jsx
+++ src/components/HomePage/ContainerEachGenre.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { makeRequest } from "../../api/api-utils";
 import { SongCard } from "./SongCard";
+import { AiFillFire } from "react-icons/ai";
 
 export const ContainerEachGenre = ({ genre }) => {
   const [songsByGenre, setSongsByGenre] = useState([]);
@@ -11,8 +12,11 @@ export const ContainerEachGenre = ({ genre }) => {
   return (
     <>
       {songsByGenre.length > 0 && (
-        <section className="mb-4 w-screen">
-          <h3 className="px-4 py-2 text-teal">{genre.toUpperCase()}</h3>
+        <section className="pt-4 w-full">
+          <h3 className="px-4 py-2 text-teal flex align-middle justify-start">
+            <AiFillFire className="self-center mr-1" />
+            {genre.toUpperCase()}
+          </h3>
           <div className="px-2 flex flex-row flex-nowrap align-middle justify-start overflow-x-scroll">
             {songsByGenre.map(({ id, name, url, thumbnail }) => (
               <SongCard
