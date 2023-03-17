e52a789f466ca22f9d66c262a0c3d995c062e532
monica-reverte <monicareverte24@gmail.com>
2023-03-17 10:53:53 +0100

    first step style likelibrary

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,8 +7,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'newblack': '#2B2B2B',
-        'teal': '#02E4E6',
+        newblack: "#100F0F",
+        teal: "#02E4E6",
+        newgray: "#282828",
+        graytext: "#424242"
       }
     },
   },
