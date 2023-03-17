fdae11e32fdcea63a1cb1983bb57007f95ad6f09
monica-reverte <monicareverte24@gmail.com>
2023-03-17 14:32:25 +0100

    mobile likelibrary

--- src/components/LikeLibrary/ContainerLikeLibrary.jsx
+++ src/components/LikeLibrary/ContainerLikeLibrary.jsx
@@ -1,40 +1,31 @@
-// import { useEffect, useState } from "react";
-// import { makeRequest } from "../../api/api-utils";
-// import { ContainerEachGenre } from "./ContainerEachGenre";
+import { useEffect, useState } from "react";
+import { makeRequest } from "../../api/api-utils";
 import React from 'react';
-
+import { EachLikeSong } from "./EachLikeSong";
 import cover from "../../assets/testimg/portada.jpg"
-import song1  from "../../assets/testimg/song1.jpg";
 import { PlayButtonLibrary } from './PlayButtonLibrary';
 
 export const ContainerLikeLibrary = () => {
+    const [likelist, setlikelist] = useState([]);
+    useEffect(() => {
+        makeRequest("playlists").then((data) => setlikelist(data));
+    }, []);
+
     return(
+
     <div className="min-h-screen h-full text-white">
-    <div>
-        <img className="rounded-b-3xl" src={cover} alt="cover" />
-    </div>
-    <div>
-        <PlayButtonLibrary />
-    </div>
-    <div className="flex flex-col m-5">
-        <div className="grid grid-rows-3 grid-flow-col gap-2">
-            <img className="rounded-2xl row-span-3" src={song1} alt="song1" />
-            <p className="col-span-1 mt-4 font-bold">Dreamhop Chill Lofi Beats</p>
-            <p className="col-span-1 text-graytext font-bold">Playlist·Dreamhop music</p>
+        <div>
+            <img className="rounded-b-3xl" src={cover} alt="cover" />
+        </div>
+        <div>
+            <PlayButtonLibrary />
+        </div>
+        <div className="flex flex-col m-5">
+            {likelist.map((song) => (
+                <EachLikeSong key={song.id} song={song}/>
+            ))}
         </div>
     </div>
-</div>
-)
-//   const [genres, setGenres] = useState([]);
-//   useEffect(() => {
-//     makeRequest("genres").then((data) => setGenres(data));
-//   }, []);
 
-//   return (
-//     <div className="pb-4">
-//       {genres.map(({ name }) => (
-//         <ContainerEachGenre key={name} genre={name} />
-//       ))}
-//     </div>
-//   );
+)
 };
