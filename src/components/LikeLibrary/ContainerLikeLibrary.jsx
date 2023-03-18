ba2214e62ea5f8613f32ea1ee49c9471f830f0d8
monica-reverte <monicareverte24@gmail.com>
2023-03-18 12:33:27 +0100

    mobile likelist finish

--- src/components/LikeLibrary/ContainerLikeLibrary.jsx
+++ src/components/LikeLibrary/ContainerLikeLibrary.jsx
@@ -2,23 +2,34 @@ import { useEffect, useState } from "react";
 import { makeRequest } from "../../api/api-utils";
 import React from 'react';
 import { EachLikeSong } from "./EachLikeSong";
-import cover from "../../assets/testimg/portada.jpg"
 import { PlayButtonLibrary } from './PlayButtonLibrary';
 
 export const ContainerLikeLibrary = () => {
     const [likelist, setlikelist] = useState([]);
+    const [photolist, setPhotolist] = useState([]);
+    
+
     useEffect(() => {
+
         makeRequest("playlists").then((data) => setlikelist(data));
+    }, []);
+
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * 11);
+        makeRequest("playlists").then((data) => setPhotolist(data[randomIndex]));
     }, []);
 
     return(
 
-    <div className="min-h-screen h-full text-white">
-        <div>
-            <img className="rounded-b-3xl" src={cover} alt="cover" />
+    <div className="min-h-screen h-full text-white flex flex-col">
+        <div className="flex justify-center">
+            <img className="rounded-b-3xl w-full" src={photolist.thumbnail} alt="cover" />
         </div>
         <div>
+            
             <PlayButtonLibrary />
+            
+
         </div>
         <div className="flex flex-col m-5">
             {likelist.map((song) => (
