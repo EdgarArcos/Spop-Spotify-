5716c1f382df7fbadcfd61a78341d2779ab96582
monica-reverte <monicareverte24@gmail.com>
2023-03-20 16:03:30 +0100

    splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { router } from "./routes/routes";
 import { RouterProvider } from "react-router-dom";
+import { SplashScreen } from "./pages";
 
 function App() {
   return (
     <div className="bg-newblack min-h-screen h-full text-white">
-      <RouterProvider router={router} />
+      <RouterProvider router={router} fallbackElement={<SplashScreen />} />
     </div>
   );
 }
