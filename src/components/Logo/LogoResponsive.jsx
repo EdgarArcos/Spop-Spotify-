c1eff15e5febc640242fcb83c6ea32f8a4b32386
Edgar Arcos <edgararcosmartin@gmail.com>
2023-03-27 07:51:15 +0000

    Logo recolocado

--- src/components/Logo/LogoResponsive.jsx
+++ src/components/Logo/LogoResponsive.jsx
@@ -5,8 +5,8 @@ import  Fulllogo  from "../../assets/testimg/fulllogo.png";
 export const LogoResponsive = () => {
   return (
     <div className='rounded-lg'>
-    <img className='absolute top-20 left-32  w-0 max-[390px]:w-36 ' src={Slogo} alt="Logo" />
-    <img className='absolute top-20  w-96 max-[390px]:w-0' src={Fulllogo} alt="Logo" />
+    <img className='absolute top-10 left-[31%]  w-0 max-[390px]:w-36 ' src={Slogo} alt="Logo" />
+    <img className='absolute top-10 left-[96]  w-96 max-[390px]:w-0' src={Fulllogo} alt="Logo" />
     </div>
   )
 }
