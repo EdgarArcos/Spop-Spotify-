9fdb93dcc2eeff234c08d73ea101487b092912ed
Edgar Arcos <edgararcosmartin@gmail.com>
2023-04-26 16:48:02 +0100

    Registro y Login en auth0 creado y logout creado

--- src/components/Login/ButtonLogin.jsx
+++ src/components/Login/ButtonLogin.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const ButtonLogin = () => {
   return (
     <div>
-    <button className='bg-transparent w-80 border-2 border-teal rounded-lg text-2xl hover:bg-teal'><p className='py-2 px-5 text-teal hover:text-black'>Log in</p></button>
+      <button className='bg-transparent w-80 border-2 border-teal rounded-lg text-2xl hover:bg-teal'><p className='py-2 px-5 text-teal hover:text-black'>Log in</p></button>
     </div>
   )
 }
