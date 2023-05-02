fc2506dd4a9289e3d0f431eaff01f6b46dadba8b
Isaura Martí <imarti.teixido@gmail.com>
2023-05-02 12:54:05 +0200

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import App from "./App";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = import.meta.env.VITE_APP_AUTH0_DOMAIN
-const clientId = import.meta.env.VITE_APP_AUTH0_CLIENT_ID
+const domain = import.meta.env.VITE_APP_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
       <App />
     </Auth0Provider>
   </React.StrictMode>
