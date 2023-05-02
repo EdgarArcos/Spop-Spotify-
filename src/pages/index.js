6fe401cf6f675c166213c7417ab2c66eca3ca50a
monica-reverte <monicareverte24@gmail.com>
2023-05-02 12:59:07 +0200

    register

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,5 @@ export * from './Home';
 export * from './Login';
 export * from './NowPlaying';
 export * from './Profile';
-export * from './Register';
 export * from './ResetPassword';
 export * from './SplashScreen';
