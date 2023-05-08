c2ffb74103215e1e109d6504e77e3866a3ccbc03
Edgar Arcos <edgararcosmartin@gmail.com>
2023-05-08 11:40:44 +0100

    fixed cerrar modal al enviar formulario

--- src/components/ArtProfile/ModalSong.jsx
+++ src/components/ArtProfile/ModalSong.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { useForm } from "react-hook-form";
 import { useSongs } from "../../context/SongContext/SongContext";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useState } from "react";
-export const ModalSong = () => {
+export const ModalSong = ({ isVisible, onClose }) => {
     const { createSong } = useSongs()
     const { register, handleSubmit } = useForm()
     const { user } = useAuth0()
@@ -16,39 +15,43 @@ export const ModalSong = () => {
             genre: data.genre,
             song: song
         })
+        onClose()
     }
+    if (!isVisible) return null
 
     return (
-        <div>
-            <h2>Formulario</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label>Title</label>
-                    <input type='text' {...register('name', {
-                        required: true,
-                        maxLength: 15
-                    })} />
-                </div>
-                <div>
-                    <label>Genre</label>
-                    <select {...register("genre")}>
-                        <option value="PoP">PoP</option>
-                        <option value="Rock">Rock</option>
-                        <option value="Soul">Soul</option>
-                        <option value="Funk">Funk</option>
-                        <option value="Folk">Folk</option>
-                        <option value="Indie">Indie</option>
-                        <option value="Electric">Electric</option>
-                    </select>
-                </div>
-                <div>
-                    <label>File</label>
-                    <input type='file' {...register('file', {
-                        required: false,
-                    })} />
-                </div>
-                <input type='submit' value="Save"></input>
-            </form>
+        <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center'>
+            <div>
+                <div className='pl-6'><button className='text-white text-xl pl-96' onClick={() => { onClose() }}>X</button></div>
+                <form onSubmit={handleSubmit(onSubmit)} className=' bg-red-700'>
+                    <div>
+                        <label>Title</label>
+                        <input type='text' {...register('name', {
+                            required: true,
+                            maxLength: 15
+                        })} />
+                    </div>
+                    <div>
+                        <label>Genre</label>
+                        <select {...register("genre")}>
+                            <option value="PoP">PoP</option>
+                            <option value="Rock">Rock</option>
+                            <option value="Soul">Soul</option>
+                            <option value="Funk">Funk</option>
+                            <option value="Folk">Folk</option>
+                            <option value="Indie">Indie</option>
+                            <option value="Electric">Electric</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label>File</label>
+                        <input type='file' {...register('file', {
+                            required: false,
+                        })} />
+                    </div>
+                    <input type='submit' value="Save"></input>
+                </form>
+            </div>
         </div>
     )
 }
--- src/pages/Artistprofile.jsx
+++ src/pages/Artistprofile.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ListSongsArtist from '../components/ArtProfile/ListSongsArtist';
 import { ModalSong } from '../components/ArtProfile/ModalSong';
 
 export const Artistprofile = () => {
+    const [showModal, setShowModal] = useState(false)
     return (
         <>
+            <div><button className=" bg-indigo-600 text-sm px-2 py-1 rounded-sm hover:bg-indigo-500 my-5" onClick={() => setShowModal(true)}>New Song +</button></div>
             <div><ListSongsArtist /></div>
-            <div><ModalSong /></div>
+            <div><ModalSong isVisible={showModal} onClose={() => setShowModal(false)} /></div>
         </>
 
     )
