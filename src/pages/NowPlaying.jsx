da34b9db2f87fcdbe605ea3c5291d35715249612
Isaura Martí <imarti.teixido@gmail.com>
2023-05-10 17:58:34 +0200

    changes NowPlaying

--- src/pages/NowPlaying.jsx
+++ src/pages/NowPlaying.jsx
@@ -1,27 +1,29 @@
-import React, { useState, useContext } from 'react'
-import { MusicContext } from '../context/MusicContext/MusicContext'
-import { FaArrowLeft } from "react-icons/fa"
-import { Link } from 'react-router-dom'
+import React, { useState, useContext } from "react";
+import { MusicContext } from "../context/MusicContext/MusicContext";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const NowPlaying = () => {
-
   const { musicState } = useContext(MusicContext);
-  const { likelist, indexPlay } = musicState;
+  const { currentList, indexPlay } = musicState;
 
   return (
     <div className="h-screen flex flex-col sm:pl-[45%] ">
-      <div>
-        <Link to="/likelibrary">
-          <FaArrowLeft className="flex text-xl mt-6 cursor-pointer"/>
-        </Link>
-      </div>
-      <div className="text-white text-center mt-10 sm:pr-[55%]">
-        <p className=" sm:pl-[18%] text-lg font-bold">{likelist[indexPlay].name}</p>
-        <p className=" sm:pl-[16%] text-gray-400">{likelist[indexPlay].artist}</p>
+      <Link to="/likelibrary">
+        <FaArrowLeft className="flex text-xl mt-6 cursor-pointer" />
+      </Link>
+
+      <div className="flex flex-col items-center justify-center text-white w-full h-full mb-48 sm:mb-32">
+        <p className=" text-lg font-bold">{currentList[indexPlay].name}</p>
+        <p className="  text-gray-400">{currentList[indexPlay].artist}</p>
         <div>
-        <img src={likelist[indexPlay].thumbnail} alt="album cover" className=" h-80 ml-9 mt-4 sm:w-[100%] sm:h-[100%]" />  
+          <img
+            src={currentList[indexPlay].img}
+            alt="album cover"
+            className="w-[15rem] h-[15rem] sm:w-[100%] sm:h-[100%]"
+          />
         </div>
       </div>
     </div>
   );
-}
+};
