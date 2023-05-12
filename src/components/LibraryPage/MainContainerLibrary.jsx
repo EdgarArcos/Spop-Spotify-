20256630e9031b5df570a782c91eae8d5a9f4d6a
Isaura Martí <imarti.teixido@gmail.com>
2023-05-12 15:10:45 +0200

    feat show info your library

--- src/components/LibraryPage/MainContainerLibrary.jsx
+++ src/components/LibraryPage/MainContainerLibrary.jsx
@@ -1,38 +1,30 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { makeRequest } from "../../api/api-utils";
 import { PlaylistCard } from "./PlaylistCard";
 import likedsongsbig from "../../assets/testimg/likedsongsbig.png";
 import { Link } from "react-router-dom";
+import { MusicContext } from "../../context/MusicContext/MusicContext";
 
 const MainContainerLibrary = () => {
-  const [playlists, setPlaylists] = useState([]);
-  const [likedSongs, setLikedSongs] = useState([]);
-
-
-  useEffect(() => {
-    makeRequest("playlists").then((data) => setPlaylists(data));
-    makeRequest("tracks").then((data) => setLikedSongs(data));
-  }, []);
+  const { musicState } = useContext(MusicContext);
+  const { playlist } = musicState;
 
   return (
     <div className="pb-4 w-screen pt-10 sm:pl-[16rem]">
       <h2 className="text-3xl m-2 ml-5">Library</h2>
       <div className="grid grid-cols-2 place-items-center gap-y-4 p-5 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
-        <Link to="/likelibrary">
-          <PlaylistCard
-            srcImg={likedsongsbig}
-            nameList="Liked Songs"
-            infoList={`Playlist - ${likedSongs.length} songs`}
-          />
-        </Link>
-        {playlists.length > 0 &&
-          playlists.map(({ id, name, thumbnail }) => (
-            <PlaylistCard
-              key={id}
-              srcImg={thumbnail}
-              nameList={name}
-              infoList="Playlist"
-            />
+        {playlist.length > 0 &&
+          playlist.map(({ _id, title, img }) => (
+            <Link
+              to={title === "Liked Songs" ? "/likelibrary" : `/playlist/${_id}`}
+            >
+              <PlaylistCard
+                key={_id}
+                srcImg={img}
+                nameList={title}
+                infoList="Playlist"
+              />
+            </Link>
           ))}
       </div>
     </div>
