00e5139fd5bb695fcda252caa80a2721cadc6ba1
Isaura Martí <imarti.teixido@gmail.com>
2023-05-12 12:43:30 +0200

    feat refetch home page

--- src/components/HomePage/ContainerAllGenres.jsx
+++ src/components/HomePage/ContainerAllGenres.jsx
@@ -1,12 +1,17 @@
 import { ContainerEachGenre } from "./ContainerEachGenre";
 import { getMusicRequest } from "../../api/api-utils";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 const ContainerAllGenres = () => {
-  const { isLoading, error, data } = useQuery({
+  const { isLoading, error, data, refetch } = useQuery({
     queryKey: ["genres"],
     queryFn: () => getMusicRequest("genres"),
   });
+  useEffect(() => {
+    refetch();
+  }, []);
+
   return (
     <div className="pb-24 w-screen min-h-screen pt-10 sm:pl-[16rem]">
       {error && <p>An error has occurred: {error.message}</p>}
