232eceb0b7652050d32241de61d4686ccfa3edbd
Isaura Martí <imarti.teixido@gmail.com>
2023-05-12 13:44:01 +0200

    fix bug artist route

--- src/routes/ProtectedArtist.jsx
+++ src/routes/ProtectedArtist.jsx
@@ -6,6 +6,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 export function ProtectedArtist({ children }) {
   const { user } = useContext(UsersContext);
   const { isLoading } = useAuth0();
-  if (isLoading && user?.role !== "Artist") return <Navigate to="/" replace />;
+  if (!isLoading && user?.role !== "Artist") return <Navigate to="/" replace />;
   return children;
 }
