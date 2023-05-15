461b6d5ef6558b4cce83e709f1afd13efcceec00
monica-reverte <monicareverte24@gmail.com>
2023-05-15 22:05:57 +0200

    edit title mobile

---
+++ src/components/CreateList/EditTitle.jsx
@@ -0,0 +1,72 @@
+import React, { useContext, useRef, useState } from 'react'
+import { FiEdit2 } from "react-icons/fi";
+import { editTitleFetch } from '../../api/playlistRequests';
+import { IoCloseOutline } from 'react-icons/io5';
+import { MusicContext } from '../../context/MusicContext/MusicContext';
+
+
+export const EditTitle = ({visible, onClose, playlist}) => {
+
+    const { handleEdit } = useContext(MusicContext);
+
+    const [currentTitle, setCurrentTitle] = useState(playlist.title);
+    const [isEditing, setIsEditing] = useState(false);
+    const inputRef = useRef();
+  
+    const startEdit = () => {
+      setIsEditing(true);
+      setTimeout(() => {
+        inputRef.current.focus();
+      }, 1);
+    };
+  
+    const saveInputChanges = async () => {
+      setIsEditing(false);
+      const res = await editTitleFetch({
+        newTitle: currentTitle,
+        playlistId: playlist._id,
+      });
+
+      if (res.data.ok) handleEdit(currentTitle, playlist._id);
+    };
+
+
+    if (!visible) return null;
+    const handleClose = () => {
+        onClose();
+    };
+  return (
+    <div  className="fixed inset-0 w-screen h-64 flex-row bg-black bg-opacity-70 flex justify-center items-center">
+        <label className="flex flex-col text-xs relative">
+        {isEditing ? (
+          <input
+            type="text"
+            value={currentTitle}
+            onChange={(e) => setCurrentTitle(e.target.value)}
+            className="flex-row items-center  text-white text-xl font-bold bg-transparent"
+            ref={inputRef}
+            onBlur={saveInputChanges}
+            onKeyDown={(e) => e.key === "Enter" && saveInputChanges()}
+          />
+        ) : (
+          <div
+            className="flex justify-center items-center text-white text-xl font-bold bg-transparent cursor-pointer"
+            onClick={startEdit}
+          >
+            {currentTitle || "Click to add a title"}
+          </div>
+        )}
+        {isEditing && (
+            <div className="flex justify-center p-3">
+          <FiEdit2
+            className="cursor-pointer text-teal text-lg"
+            onClick={startEdit}
+          />
+          </div>
+        )}
+    </label>
+    <div onClick={handleClose} className="flex m-4 text-xl">
+        <IoCloseOutline />
+    </div></div>
+  )
+}
--- src/components/Reusable/MyMenuModal.jsx
+++ src/components/Reusable/MyMenuModal.jsx
@@ -1,57 +1,85 @@
 
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { createplaylistFetch, deletePlaylistFetch } from '../../api/playlistRequests';
 import { MusicContext } from '../../context/MusicContext/MusicContext';
 import { useNavigate } from 'react-router-dom';
 import { UsersContext } from '../../context/UsersContext';
+import { EditTitle } from '../CreateList/EditTitle';
+import { IoCloseOutline } from 'react-icons/io5';
 
 export const MyMenuModal = ({ visible, onClose, playlist }) => {
     
     const { user } = useContext(UsersContext);
 
-    const {musicState, handleAddPlaylist, handleDeletePlaylist} = useContext(MusicContext)
+    const { musicState, handleAddPlaylist, handleDeletePlaylist } = useContext(MusicContext);
     const navigate = useNavigate();
 
+    const [showModal, setShowModal] = useState(false);
+    const [showMenuModal, setShowMenuModal] = useState(true); 
 
     const handleClose = () => {
-        onClose()
-    }
+        onClose();
+    };
 
     if (!visible) return null;
 
-    const deletePlaylist = async() =>{
+    const deletePlaylist = async () => {
         const response = await deletePlaylistFetch(playlist._id);
-        console.log(response)
-        if(response.data.ok) {
-            handleDeletePlaylist(playlist._id)
+        console.log(response);
+        if (response.data.ok) {
+            handleDeletePlaylist(playlist._id);
             navigate("/");
         }
-
-    }
+    };
 
     const handleCreate = async () => {
-      const res = await createplaylistFetch(user._id);
-      if (res.data.ok) {
-        handleAddPlaylist(res.data.playlist);
-        navigate(`/playlist/${res.data.playlist._id}`);
-      }
+        const res = await createplaylistFetch(user._id);
+        if (res.data.ok) {
+            handleAddPlaylist(res.data.playlist);
+            navigate(`/playlist/${res.data.playlist._id}`);
+        }
     };
-  
 
+    const handleEditPlaylist = () => {
+        setShowModal(true);
+        setShowMenuModal(false);
+    };
 
-
-return (
-    <div onClick={handleClose} className="fixed ml-16 mt-52 bg-black bg-opacity-30 flex justify-center items-center">
-        <ul className="bg-gradient-to-b from-zinc-700 to-zinc-900 smborder-b border-newblack border-2  p-6 ">
-            <li className="p-2  hover:bg-newgray rounded-md cursor-pointer">Go premium</li>
-            <li className="p-2  hover:bg-newgray rounded-md cursor-pointer" onClick={handleCreate}>Create Playlist</li>
-            <li className="p-2  hover:bg-newgray rounded-md cursor-pointer" onClick={deletePlaylist}>Delete playlist</li>            
-            <li className="p-2  hover:bg-newgray rounded-md cursor-pointer">Go home</li>
-            <li className="p-2 hover:bg-newgray rounded-md cursor-pointer">Share</li>
-            
-
-        </ul>
-    </div>
-    )
-}
+    const handleCloseEditModal = () => {
+        setShowModal(false);
+        setShowMenuModal(true);
+      };
+      return (
+        <div>
+          {showMenuModal && (
+            <div
+              
+              className="fixed inset-0 top-0  bg-black bg-opacity-30 flex justify-center items-center"
+            >
+              <ul className="bg-gradient-to-b from-zinc-700 to-zinc-900 smborder-b border-newblack border-2  p-6 ">
+                <li className="p-2  hover:bg-newgray rounded-md cursor-pointer" onClick={handleCreate}>
+                  Create Playlist
+                </li>
+                <li className="p-2  hover:bg-newgray rounded-md cursor-pointer" onClick={deletePlaylist}>
+                  Delete playlist
+                </li>
+                <li className="p-2  hover:bg-newgray rounded-md cursor-pointer sm:hidden" onClick={handleEditPlaylist}>
+                  Edit Playlist
+                </li>
+                <li onClick={handleClose} className="flex p-2 justify-center text-lg hover:bg-newgray rounded-md cursor-pointer">
+                <IoCloseOutline />
+                </li>
+              </ul>
+            </div>
+          )}
+          {showModal && (
+            <EditTitle
+              onClose={handleCloseEditModal}
+              visible={showModal}
+              playlist={playlist}
+            />
+          )}
+        </div>
+      );
+    };
 
