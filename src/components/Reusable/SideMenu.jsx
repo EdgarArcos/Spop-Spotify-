e31334cc8e54bd1b6ec17cb416a7ce0a1857a776
Isaura Martí <imarti.teixido@gmail.com>
2023-05-16 10:23:26 +0200

    fix bug signup

--- src/components/Reusable/SideMenu.jsx
+++ src/components/Reusable/SideMenu.jsx
@@ -1,15 +1,15 @@
-import React, { useContext, useEffect, useState } from "react";
-import fulllogo from "../../assets/testimg/fulllogo.png";
-import likedsongs from "../../assets/testimg/likedsongs.png";
-import { AiFillHome, AiOutlineSearch } from "react-icons/ai";
-import { BiLibrary } from "react-icons/bi";
-import { FaUserCircle } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
-import { BsFillPlusSquareFill } from "react-icons/bs";
-import { UsersContext } from "../../context/UsersContext";
-import { createplaylistFetch } from "../../api/playlistRequests";
-import { MusicContext } from "../../context/MusicContext/MusicContext";
-import { MdOutlineLibraryMusic } from "react-icons/md"
+import React, { useContext, useEffect, useState } from 'react';
+import fulllogo from '../../assets/testimg/fulllogo.png';
+import likedsongs from '../../assets/testimg/likedsongs.png';
+import { AiFillHome, AiOutlineSearch } from 'react-icons/ai';
+import { BiLibrary } from 'react-icons/bi';
+import { FaUserCircle } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { BsFillPlusSquareFill } from 'react-icons/bs';
+import { UsersContext } from '../../context/UsersContext';
+import { createplaylistFetch } from '../../api/playlistRequests';
+import { MusicContext } from '../../context/MusicContext/MusicContext';
+import { MdOutlineLibraryMusic } from 'react-icons/md';
 
 export const SideMenu = () => {
   const { user } = useContext(UsersContext);
@@ -24,55 +24,58 @@ export const SideMenu = () => {
       navigate(`/playlist/${res.data.playlist._id}`);
     }
   };
-
+  console.log(user);
+  console.log(playlist);
   return (
-    <div className='flex flex-col h-screen bg-newblack p-4 w-60 fixed'>
-      <img className='pb-3' src={fulllogo} alt='logo' />
+    <div className="flex flex-col h-screen bg-newblack p-4 w-60 fixed">
+      <img className="pb-3" src={fulllogo} alt="logo" />
       <Link
-        to='/'
-        className='flex flex-row pl-2 p-1 hover:bg-newgray rounded-md cursor-pointer'
+        to="/"
+        className="flex flex-row pl-2 p-1 hover:bg-newgray rounded-md cursor-pointer"
       >
-        <AiFillHome className='text-xl' />
-        <p className='text-md pl-2'>Home</p>
+        <AiFillHome className="text-xl" />
+        <p className="text-md pl-2">Home</p>
       </Link>
       <Link
-        to='/profile'
-        className='flex flex-row pl-2 p-1 hover:bg-newgray rounded-md cursor-pointer'
+        to="/profile"
+        className="flex flex-row pl-2 p-1 hover:bg-newgray rounded-md cursor-pointer"
       >
-        <FaUserCircle className='text-xl' />
-        <p className='text-md pl-2'>Profile</p>
+        <FaUserCircle className="text-xl" />
+        <p className="text-md pl-2">Profile</p>
       </Link>
       <Link
-        to='/search'
-        className='flex flex-row pl-2 p-1 hover:bg-newgray rounded-md cursor-pointer'
+        to="/search"
+        className="flex flex-row pl-2 p-1 hover:bg-newgray rounded-md cursor-pointer"
       >
-        <AiOutlineSearch className='text-xl' />
-        <p className='text-md pl-2'>Search</p>
+        <AiOutlineSearch className="text-xl" />
+        <p className="text-md pl-2">Search</p>
       </Link>
       <Link
-        to='/library'
-        className='flex flex-row pl-2 p-1 hover:bg-newgray rounded-md cursor-pointer'
+        to="/library"
+        className="flex flex-row pl-2 p-1 hover:bg-newgray rounded-md cursor-pointer"
       >
-        <BiLibrary className='text-xl' />
-        <p className='pl-2 text-md'>Your Library</p>
+        <BiLibrary className="text-xl" />
+        <p className="pl-2 text-md">Your Library</p>
       </Link>
-      <Link to="/artist"  className="flex flex-row pl-2 p-1 hover:bg-newgray rounded-md cursor-pointer">
-
-      <MdOutlineLibraryMusic className="text-xl" />
+      <Link
+        to="/artist"
+        className="flex flex-row pl-2 p-1 hover:bg-newgray rounded-md cursor-pointer"
+      >
+        <MdOutlineLibraryMusic className="text-xl" />
         <p className="pl-2 text-md">Add song</p>
       </Link>
-      <div className='flex flex-row p-2'>
-        <p className='text-graytext py-2'>PLAYLIST</p>
+      <div className="flex flex-row p-2">
+        <p className="text-graytext py-2">PLAYLIST</p>
       </div>
 
       <div
         onClick={handleCreate}
-        className='flex flex-row p-2  hover:bg-newgray rounded-md cursor-pointer'
+        className="flex flex-row p-2  hover:bg-newgray rounded-md cursor-pointer"
       >
         <div>
-          <BsFillPlusSquareFill className='text-2xl mr-3' />
+          <BsFillPlusSquareFill className="text-2xl mr-3" />
         </div>
-        <p className='ml-2'>Create Playlist</p>
+        <p className="ml-2">Create Playlist</p>
       </div>
 
       {playlist.length > 0 &&
@@ -83,15 +86,15 @@ export const SideMenu = () => {
               p.title === 'Liked Songs' ? '/likelibrary' : `/playlist/${p._id}`
             }
           >
-            <div className='flex flex-row p-2  hover:bg-newgray rounded-md cursor-pointer'>
+            <div className="flex flex-row p-2  hover:bg-newgray rounded-md cursor-pointer">
               <div>
                 <img
-                  className='w-7 h-7 rounded mr-2'
+                  className="w-7 h-7 rounded mr-2"
                   src={p.img}
-                  alt='playlist'
+                  alt="playlist"
                 />
               </div>
-              <p className='ml-2'>{p.title}</p>
+              <p className="ml-2">{p.title}</p>
             </div>
           </Link>
         ))}
