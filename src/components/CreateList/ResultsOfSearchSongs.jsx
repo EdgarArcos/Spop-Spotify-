71484b58775eea18c4342f270b0af4daa772eda7
Isaura Martí <imarti.teixido@gmail.com>
2023-05-16 10:43:09 +0200

    fix warnings keys

--- src/components/CreateList/ResultsOfSearchSongs.jsx
+++ src/components/CreateList/ResultsOfSearchSongs.jsx
@@ -1,7 +1,7 @@
-import { useContext, useState } from "react";
-import { addSongPlaylistFetch } from "../../api/playlistRequests";
-import { MusicContext } from "../../context/MusicContext/MusicContext";
-import { useScreenWidth } from "../../hooks/useScreenWidth";
+import { useContext, useState } from 'react';
+import { addSongPlaylistFetch } from '../../api/playlistRequests';
+import { MusicContext } from '../../context/MusicContext/MusicContext';
+import { useScreenWidth } from '../../hooks/useScreenWidth';
 
 export const ResultsOfSearchSongs = ({ resultsArr, playlistId }) => {
   const screenWidth = useScreenWidth();
@@ -21,56 +21,54 @@ export const ResultsOfSearchSongs = ({ resultsArr, playlistId }) => {
   return (
     <div>
       {resultsArr.map(({ _id, artist, name, img }) => (
-        <div>
-           {screenWidth < 640 ? (
-           <div
-           key={_id}
-           className="grid grid-cols-3  w-full hover:bg-newgray rounded-md cursor-pointer m-3"
-         >
-           <img
-             className="grid items-center ml-6 rounded-2xl h-14 w-14"
-             src={img}
-             alt={name}
-           />
-   
-           <p className="grid items-center text-sm text-graytext font-bold">
-             {name}
-           </p>
-   
-           <button
-             onClick={() => handleAddToPlaylist(_id)}
-             className="flex flex-row p-2 m-2 items-center bg-teal w-10 h-10 rounded-full justify-center"
-           >
-             +
-           </button>
-         </div>
-      ) : (
-        <div
-        key={_id}
-        className="grid grid-cols-3 gap-2 w-full hover:bg-newgray rounded-md cursor-pointer m-4"
-      >
-        <img
-          className="grid items-center rounded-2xl w-20 max-w-none"
-          src={img}
-          alt={name}
-        />
+        <div key={_id}>
+          {screenWidth < 640 ? (
+            <div
+              key={_id}
+              className="grid grid-cols-3  w-full hover:bg-newgray rounded-md cursor-pointer m-3"
+            >
+              <img
+                className="grid items-center ml-6 rounded-2xl h-14 w-14"
+                src={img}
+                alt={name}
+              />
 
-        <p className="grid items-center text-graytext font-bold">
-          {name}-{artist}
-        </p>
+              <p className="grid items-center text-sm text-graytext font-bold">
+                {name}
+              </p>
 
-        <button
-          onClick={() => handleAddToPlaylist(_id)}
-          className="flex flex-row m-6 p-2 items-center bg-teal w-24 rounded-full justify-center"
-        >
-          Add
-        </button>
-      </div>
-      )}
-       
+              <button
+                onClick={() => handleAddToPlaylist(_id)}
+                className="flex flex-row p-2 m-2 items-center bg-teal w-10 h-10 rounded-full justify-center"
+              >
+                +
+              </button>
+            </div>
+          ) : (
+            <div
+              key={_id}
+              className="grid grid-cols-3 gap-2 w-full hover:bg-newgray rounded-md cursor-pointer m-4"
+            >
+              <img
+                className="grid items-center rounded-2xl w-20 max-w-none"
+                src={img}
+                alt={name}
+              />
+
+              <p className="grid items-center text-graytext font-bold">
+                {name}-{artist}
+              </p>
+
+              <button
+                onClick={() => handleAddToPlaylist(_id)}
+                className="flex flex-row m-6 p-2 items-center bg-teal w-24 rounded-full justify-center"
+              >
+                Add
+              </button>
+            </div>
+          )}
         </div>
       ))}
     </div>
   );
 };
-
