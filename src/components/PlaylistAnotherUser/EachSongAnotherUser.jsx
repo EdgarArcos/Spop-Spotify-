d1af63abfc58c70a50402d7378e8711f321ad88f
monica-reverte <monicareverte24@gmail.com>
2023-05-16 17:26:49 +0200

    responsive eachsonganother

--- src/components/PlaylistAnotherUser/EachSongAnotherUser.jsx
+++ src/components/PlaylistAnotherUser/EachSongAnotherUser.jsx
@@ -16,7 +16,7 @@ export const EachSongAnotherUser = ({ song, index }) => {
   };
 
   return (
-    <div>
+    <>
     {screenWidth < 640 ? (
       
       
@@ -45,22 +45,28 @@ export const EachSongAnotherUser = ({ song, index }) => {
         
     </div>
     ) : (
-    <tbody key={_id} onClick={() => handlePlay(index)}>
+      <tbody key={_id} onClick={() => handlePlay(index)}>
       <tr className="group/item flex hover:bg-newgray rounded-md cursor-pointer sm:grid sm:grid-cols-2 md:grid md:grid-cols-3 lg:grid lg:grid-cols-4 gap-4 sm:justify-center m-3">
-        <td className="flex justify-center pt-6">
+        <td className="hidden sm:flex justify-center pt-6">
           <p className="visible group-hover/item:invisible">{index + 1}</p>
           <FaPlay className="invisible group-hover/item:visible flex justify-center" />
         </td>
-        <td className="grid grid-cols-2">
-
+        <td className="flex sm:grid sm:grid-cols-2">
+          <div className="flex sm:hidden">
+            <img className="rounded-2xl w-20 h-20 max-w-none" src={img} alt={name} />
+            <div className="flex-row ml-3 items-center">
+              <p className="font-bold">{name}</p>
+              <p className="text-graytext font-bold">Playlist · {artist} </p>
+            </div>
+          </div>
           <img
-            className="grid col-span-1 rounded-2xl w-20 max-w-none"
+            className="hidden sm:grid sm:col-span-1 rounded-2xl w-20 h-20 max-w-none"
             src={img}
             alt={name}
           />
-          <p className="grid col-span-1">{name}</p>
+          <p className="hidden sm:grid sm:col-span-1">{name}</p>
         </td>
-        <td className="hidden md:grid  pt-6 justify-start pl-14 font-bold text-graytext">
+        <td cclassName="hidden md:grid  pt-6 justify-start pl-14 font-bold text-graytext">
           Playlist · {artist}
         </td>
         <td className="hidden lg:grid  pt-6 justify-start">
@@ -74,6 +80,6 @@ export const EachSongAnotherUser = ({ song, index }) => {
       </tr>
     </tbody>
 )}
-    </div>
+    </>
   );
 };
